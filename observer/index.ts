5c91ad82b90acf9b45f1533284d3affa5d26ae30
YourfriendRND <kupeirc@gmail.com>
2022-09-18 15:24:05 +0300

    Окончательный вариант для Observer

--- observer/index.ts
+++ observer/index.ts
@@ -9,23 +9,23 @@
 
 interface Subject {
     // Метод регистрации наблюдателя
-    registerObserver (observer: Observer):void;
-    
+    registerObserver(observer: Observer): void;
+
     // Метод исключения наблюдателя
-    removeObserver (observer: Observer):void;
+    removeObserver(observer: Observer): void;
 
     // Метод оповещения наблюдателей
-    notifyObservers ():void;
+    notifyObservers(): void;
 };
 
 interface Observer {
     // Метод для обновления состояния погодной станции
-    update (temperature: number, humidity: number, pressure: number):void; 
+    update(): void;
 };
 
 interface DisplayElement {
     // Метод для вывода данных на экраны погодной станции
-    display ():void;
+    display(): void;
 };
 
 // Реализация интерфейсов
@@ -47,13 +47,13 @@ class WeatherData implements Subject {
     removeObserver(observer: Observer): void {
         // Если наблюдатель отписывается, то фильтруем массив и возвращаем только тех наблюдателей, у которых не совпадает ссылка
         // с отписывающимся объектом 
-        this.observers = this.observers.filter((element) => element !== observer); 
+        this.observers = this.observers.filter((element) => element !== observer);
     };
 
     notifyObservers(): void {
         // При уведомлении подписчиков вызывается метод update
         // который реализуется всеми подписчиками
-        this.observers.forEach((observer) => observer.update(this.temperature, this.humidity, this.pressure)); 
+        this.observers.forEach((observer) => observer.update());
     };
 
     measurementsChanged(): void {
@@ -70,8 +70,86 @@ class WeatherData implements Subject {
     };
 
     // Могут быть другие методы...
+    // например, get методы получения конкретных данных
+
+    getTemperature():number {
+        return this.temperature;
+    };
+
+    getHumidity():number {
+        return this.humidity;
+    };
+
+    getPressure():number {
+        return this.pressure;
+    };
+
+};
+
+// Класс для визуализации текущего состояния погоды
+
+class CurrentConditionsDisplay implements Observer, DisplayElement {
+
+    // Экран с текущем состоянием погоды отображает только температуру и влажность
+
+    private temperature: number;
+    private humidity: number;
+    private weatherData: WeatherData;
+
+    // конструктору передается объект WeatherData, который используется
+    // для регистрации элемента в качестве наблюдателя
+
+    constructor(weatherData: WeatherData) {
+        this.weatherData = weatherData;
+        this.weatherData.registerObserver(this);
+    }
+
+    update(): void {
+        this.temperature = this.weatherData.getTemperature();
+        this.humidity = this.weatherData.getHumidity();
+
+        // При вызове update мы сохраняем значение температуры и влажности
+        // после чего вызываем метод для их отображения display
+
+        this.display()
+    };
+
+    display(): void {
+        console.log(`Текущее состояние погоды: 
+            Температура: ${this.temperature} градусов C,
+            Влажность: ${this.humidity} % `
+        )
+    };
+}; 
+
+
+// Формируем все объекты в единый объект программы \
+
+class WeatherStation {
+    weatherData: WeatherData;
+    currentDisplay: CurrentConditionsDisplay
+
+    constructor () {
+        this.weatherData = new WeatherData();
+        this.currentDisplay = new CurrentConditionsDisplay(this.weatherData);
+    };
+
+    // данынй метод имитирует работу погодной станции
+    // передавая данные с определенной задержкой
+    
+    work():void {
+        // Имитация получения новых данных
+        this.weatherData.setMeasurements(25, 34, 756);
+        setTimeout(() => {
+            this.weatherData.setMeasurements(30, 29, 759);
+        }, 10000);
+        setTimeout(() => {
+            this.weatherData.setMeasurements(33, 20, 761);
+        }, 20000);
+    };
+
 }
 
-
-
-
+// Запускаем станцию в тестовую работу
+const station = new WeatherStation();
+station.work();
